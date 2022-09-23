7126451b2e8d78b9439bad24e8e85bdab1e22c24
StefanKostenko <kostenkostephan@gmail.com>
2022-09-23 12:27:57 +0200

    filter finalizado

---
+++ filter.php
@@ -0,0 +1,31 @@
+<?php
+$contactos = array(
+
+    array("codigo" => 1, "nombre" => "Juan Pérez",
+    "telefono" => "966112233", "email" => "juanp@gmail.com"),
+
+    array("codigo" => 2, "nombre" => "Ana López",
+    "telefono" => "965667788", "email" => "anita@hotmail.com"),
+
+    array("codigo" => 3, "nombre" => "Mario Montero",
+    "telefono" => "965929190", "email" => "mario.mont@gmail.com"),
+
+    array("codigo" => 4, "nombre" => "Laura Martínez",
+    "telefono" => "611223344", "email" => "lm2000@gmail.com"),
+
+    array("codigo" => 5, "nombre" => "Nora Jover",
+    "telefono" => "638765432", "email" => "norajover@hotmail.com"),
+    );
+
+$filtrados = [];
+
+$filtrarPor = strtolower($_GET["dominio"] ?? "");
+
+    foreach($contactos as $contacto){
+        if(strpos(strtolower($contacto["email"]), $filtrarPor) !== FALSE){
+            $filtrados[] = $contacto;
+        }
+    }
+
+    print_r($filtrados);
+?>

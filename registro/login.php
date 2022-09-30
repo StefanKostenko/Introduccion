07d6e1fdbc934692ca1704ec0b92e306fb2f008e
StefanKostenko <kostenkostephan@gmail.com>
2022-09-30 13:39:28 +0200

    login funcionando

--- registro/login.php
+++ registro/login.php
@@ -1,5 +1,55 @@
 <?php
+$errores = [];
+if (sizeof($_POST) != 0){
+    // Conectar base de datos
+    $opciones = array(
 
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+    
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    
+        PDO::ATTR_PERSISTENT => true
+    
+    );
+    
+    $pdo = new PDO(
+    
+        'mysql:host=localhost;dbname=users;charset=utf8',
+    
+        'root',
+    
+        'sa',
+    
+    $opciones);
+
+    //Datos
+    $nombre = $_POST['nombre'];
+    $password = $_POST['password'];
+
+    //Comprovacion de errores
+    if(strlen($nombre) == 0){
+        $errores[] = "Introduzca un usuario!";
+    }
+
+    if(strlen($password) == 0){
+        $errores[] = "Introduzca la contraseña";
+    }
+
+    // Si no hay errores que ejecute la consulta
+    if(sizeof($errores) == 0){
+        $usuario = $pdo->query("SELECT username, email FROM users WHERE username = '$nombre' and password = '$password'");
+        
+        $usuario->execute();
+    }
+
+    // Comprovacion rapida si ha entrado o no.
+    if ($registro = $usuario->fetch()){
+            echo "Entrado";
+    }else{
+        $errores[] = "El usuario o contraseña no coinciden!";
+    }
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +61,21 @@
     <title>Login</title>
 </head>
 <body>
+    <form method="POST">
         <label>Nombre usuari@: </label><br>
         <input type="text" id="nombre" name="nombre" value=""/></input><br>
-        <label>Correo: </label><br>
-        <input type="text" id="correo" name="correo" value=""/></input><br><br>
+        <label>Contraseña: </label><br>
+        <input type="text" id="password" name="password" value=""/></input><br><br>
         <input type="submit" name="submit" value="Entrar"/><br><br>
+    </form>
+
+    <?php
+        if (sizeof($errores) > 0 ){
+            foreach ($errores as $error){
+                echo $error . "<br>";
+            }
+        }
+    ?>
+
 </body>
 </html>
